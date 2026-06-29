759fa48905be5d2be4e410625ae7e8b789833718
kimoade <130127406+kimoade@users.noreply.github.com>
2026-06-29 05:46:54 +0000

    Finished receipt calculator

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include "receipt.hpp"
-// #include <string> // might need later
+
 
 using namespace std;
 
@@ -25,21 +25,20 @@ int main() {
     cout << "Enter tip percentage: ";
     cin >> tip_amount;
 
-    // do all the math here using our functions
     double sub = calculateSubtotal(p1, p2, p3);
     double myTax = calculateTax(sub);
     double myTip = calculateTip(sub, tip_amount);
     double total_price = calculateTotal(sub, myTax, myTip);
 
-    // format it so it looks like real money (2 decimals)
+    
     cout << fixed;
     cout << setprecision(2);
     
-    cout << endl; // extra space
+    cout << endl; 
     cout << "Subtotal: $" << sub << endl;
     cout << "Tax: $" << myTax << endl;
     cout << "Tip: $" << myTip << endl;
     cout << "Total: $" << total_price << "\n";
 
-    return 0; // program finished
+    return 0; 
 }
